90e5fc4ce8705ce51480c099843b759df2dd9e21
sarthak soni <sarthak.soni@iiitb.org>
2017-05-17 11:47:42 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,7 +1,7 @@
 import os
 from fnmatch import fnmatch
 
-root = "/media/sarthak/Windows/Movies"
+root = "/media/sarthak/Windows/Movies"#Path of your movies folder
 listofpattern = ["*.mp4",".avi",".mkv"]
 listofmovies = []
 
